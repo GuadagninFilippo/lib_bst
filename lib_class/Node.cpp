a1b6d572651f34433d747f0946a378e5b4760de3
GuadagninFilippo <filippogda13@gmail.com>
2024-11-28 18:51:50 +0100

    Update Node.cpp

--- lib_class/Node.cpp
+++ lib_class/Node.cpp
@@ -24,6 +24,76 @@ Node* Node::insertR(int k) {
     return this;
 }
 
+// insert non ricorsivo
+Node* Node::insertI(int k) {
+    Node* Current = this;
+
+    while (true) {
+        if (k == Current->data) {
+            Current->weight++; 
+            break;
+        
+        } else if (k < Current->data) {
+            if (Current->lchild == nullptr) {
+                Current->lchild = new Node(k); 
+                break;
+            } else {
+                Current=Current->lchild;
+            }
+        } else { 
+            if (Current->rchild == nullptr) {
+                Current->rchild = new Node(k); 
+                break;
+
+            } else {
+                Current=Current->rchild;
+            }
+        }
+    }
+}
+
+//search ricorsivo
+bool Node::searchR(int k) {
+    if (k == data) {
+        return true; 
+    }
+    if (this == nullptr) {
+        return false;
+    }
+    if (k < data) {
+        if (lchild != nullptr) {
+            return lchild->searchR(k);
+        } else {
+            return false;
+        }
+    } else { 
+        if (rchild != nullptr) {
+            return rchild->searchR(k);
+        } else {
+            return false;
+        }
+    }
+}
+
+// search iterattivo
+bool Node::searchI(int k) {
+
+    Node* current = this;
+     
+    while (current != nullptr) {
+        if (k == current->data) {
+            return true;
+
+        } else if (k < current->data) {
+            
+            current = current->lchild;
+        } else {
+            current = current->rchild;
+        }
+    }
+    return false; 
+}
+
 void Node::inOrder() {
 
     if (lchild != nullptr){
